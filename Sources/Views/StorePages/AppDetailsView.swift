346164803108dea4d6dcaa8cc5becfa454b1f7ab
Armin <armin536@gmail.com>
2025-10-15 19:56:20 +0330

    Updated AppDetailsView

--- Sources/Views/StorePages/AppDetailsView.swift
+++ Sources/Views/StorePages/AppDetailsView.swift
@@ -8,18 +8,18 @@
 import NukeUI
 import SwiftUI
 
-struct AppDetailsView {
+struct AppDetailsView: View {
     public init(
         icon: String,
         name: String,
         subtitle: String,
         description: String,
         details: [AppInfo],
-        detailsSectionTitle: LocalizedStringKey = "Details",
+        detailsSectionTitle: LocalizedStringResource = "Details",
         screenshots: [URL],
         screenshotAspectRatio: CGFloat?,
-        screenshotsSectionTitle: LocalizedStringKey = "Screenshots",
-        installTitle: LocalizedStringKey,
+        screenshotsSectionTitle: LocalizedStringResource = "Screenshots",
+        installTitle: LocalizedStringResource,
         installAction: @escaping () -> Void
     ) {
         self.icon = icon
@@ -40,20 +40,18 @@ struct AppDetailsView {
     public var subtitle: String
     public var description: String
     public var details: [AppInfo]
-    public var detailsSectionTitle: LocalizedStringKey
+    public var detailsSectionTitle: LocalizedStringResource
     public var screenshots: [URL]
     public var screenshotAspectRatio: CGFloat
-    public var screenshotsSectionTitle: LocalizedStringKey
-    public var installTitle: LocalizedStringKey
+    public var screenshotsSectionTitle: LocalizedStringResource
+    public var installTitle: LocalizedStringResource
     public var installAction: () -> Void
-}
-
-extension AppDetailsView {
+    
     private var iconSize: CGFloat {
         #if os(watchOS)
         return 64
         #else
-        return 125
+        return 100
         #endif
     }
     
@@ -61,7 +59,7 @@ extension AppDetailsView {
         #if os(watchOS) || os(visionOS)
         return Circle()
         #else
-        return RoundedRectangle(cornerRadius: 27)
+        return RoundedRectangle(cornerRadius: 24)
         #endif
     }
     
@@ -72,9 +70,7 @@ extension AppDetailsView {
         return 250
         #endif
     }
-}
-
-extension AppDetailsView: View {
+    
     public var body: some View {
         List {
             appInfoSection
@@ -84,7 +80,6 @@ extension AppDetailsView: View {
         }
     }
     
-    @MainActor
     private var appInfoSection: some View {
         Section {
             #if os(watchOS)
@@ -119,28 +114,28 @@ extension AppDetailsView: View {
             
         }
         .listRowInsets(.init())
+        .listRowSeparator(.hidden)
         .listRowBackground(Color.clear)
         #if os(macOS)
         .padding(.bottom)
         #endif
     }
     
-    @MainActor
     private var appDetailsSection: some View {
         Section {
             appInfoView
                 .listRowInsets(.init())
         }
+        .listRowSeparator(.hidden)
     }
     
-    @MainActor
     private var screenshotsSection: some View {
         Section(screenshotsSectionTitle) {
             screenshotsView
-                .listRowInsets(.init())
                 .listRowBackground(Color.clear)
         }
         .headerProminence(.increased)
+        .listRowSeparator(.hidden)
     }
     
     private var detailsSection: some View {
@@ -148,12 +143,10 @@ extension AppDetailsView: View {
             Text(description)
         }
         .headerProminence(.increased)
+        .listRowSeparator(.hidden)
     }
-}
-
-extension AppDetailsView {
-    @MainActor
-    var iconImage: some View {
+    
+    private var iconImage: some View {
         LazyImage(url: URL(string: icon)) { state in
             if let image = state.image {
                 image
@@ -170,9 +163,9 @@ extension AppDetailsView {
         .clipShape(iconShape)
     }
     
-    var nameText: some View {
+    private var nameText: some View {
         Text(name)
-            .font(.title3)
+            .font(.title2)
             .fontWeight(.bold)
             .foregroundStyle(.primary)
             .multilineTextAlignment(.leading)
@@ -180,7 +173,7 @@ extension AppDetailsView {
             .frame(maxWidth: .infinity, alignment: .leading)
     }
     
-    var subtitleText: some View {
+    private var subtitleText: some View {
         Text(subtitle)
             .font(.callout)
             .foregroundStyle(.secondary)
@@ -189,65 +182,52 @@ extension AppDetailsView {
             .frame(maxWidth: .infinity, alignment: .leading)
     }
     
-    @MainActor
-    var installButton: some View {
+    private var installButton: some View {
         Button(action: installAction) {
             Text(installTitle)
-                .frame(minWidth: 60)
-                .font(.body.weight(.semibold))
-                #if os(macOS)
-                .padding(.vertical, 5)
-                #endif
+                .font(.body)
+                .fontWeight(.bold)
         }
         .buttonStyle(.borderedProminent)
-        .buttonBorderShape(.roundedRectangle)
         #if os(macOS)
-        .apply {
-            if #available(macOS 14.0, *) {
-                $0.clipShape(.capsule)
-            } else {
-                $0
-            }
-        }
+        .controlSize(.large)
         #else
-        .clipShape(.capsule)
-        .controlSize(.mini)
+        .controlSize(.regular)
         #endif
     }
     
-    @MainActor
-    var appInfoView: some View {
-        ScrollView(.horizontal) {
-            HStack {
-                ForEach(details) { detail in
-                    VStack {
-                        Label(detail.title, systemImage: detail.icon)
-                                .font(.caption)
-                            
-                            Text(detail.subtitle)
-                                .font(.body)
-                                .fontWeight(.bold)
+    private var appInfoView: some View {
+        HStack {
+            ForEach(details) { detail in
+                VStack {
+                    HStack {
+                        Image(systemName: detail.icon)
+                        Text(detail.title)
+                            .lineLimit(1)
+                            .fixedSize()
                     }
-                    .lineLimit(1)
-                    .padding(.horizontal)
+                    .font(.caption)
                     
-                    if detail != details.last {
-                        Divider()
-                    }
+                    Text(detail.subtitle)
+                        .font(.body)
+                        .fontWeight(.bold)
+                        .lineLimit(1)
+                        .fixedSize()
+                }
+                .lineLimit(1)
+                .padding(.horizontal)
+                .frame(maxWidth: .infinity)
+                
+                if detail != details.last {
+                    Divider()
                 }
             }
-            .foregroundStyle(.secondary)
-            .padding()
         }
-        .apply {
-            if #available(iOS 17.0, macOS 14.0, *) {
-                $0.scrollTargetBehavior(.viewAligned)
-            } else { $0 }
-        }
+        .foregroundStyle(.secondary)
+        .padding()
     }
     
-    @MainActor
-    var screenshotsView: some View {
+    private var screenshotsView: some View {
         ScrollView(.horizontal) {
             HStack {
                 ForEach(screenshots, id: \.self) { screenshot in
@@ -270,15 +250,9 @@ extension AppDetailsView {
                 }
             }
         }
-        .apply {
-            if #available(iOS 17.0, macOS 14.0, *) {
-                $0.scrollTargetBehavior(.paging)
-            } else { $0 }
-        }
     }
 }
 
-@available(iOS 16.0, macOS 13.0, *)
 #Preview {
     NavigationStack {
         AppDetailsView(
@@ -309,7 +283,7 @@ extension AppDetailsView {
                 URL(string: "https://api.seebaro.ir/media/screenshots/4d0f7c19-7941-4166-a149-b031a22168c6.png")!,
             ],
             screenshotAspectRatio: 0.4612676056,
-            installTitle: "Get",
+            installTitle: "Install",
             installAction: {}
         )
         .navigationTitle("Details")
