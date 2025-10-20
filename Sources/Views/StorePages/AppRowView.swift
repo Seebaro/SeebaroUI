59f335b70f5c0ed87fd5696cbc203c63bcf3442c
Armin <armin536@gmail.com>
2025-10-20 20:35:50 +0330

    Added AppRowView

---
+++ Sources/Views/StorePages/AppRowView.swift
@@ -0,0 +1,81 @@
+//
+//  AppRowView.swift
+//  SeebaroUI
+//
+//  Created by Armin on 10/15/25.
+//
+
+import NukeUI
+import SwiftUI
+
+public struct AppRowView: View {
+    public init(
+        title: String,
+        subtitlte: String,
+        icon: String,
+        downloadAction: @escaping () -> Void
+    ) {
+        self.title = title
+        self.subtitlte = subtitlte
+        self.icon = icon
+        self.downloadAction = downloadAction
+    }
+    
+    let title: String
+    let subtitlte: String
+    let icon: String
+    let downloadAction: () -> Void
+    
+    public var body: some View {
+        HStack(spacing: 15) {
+            LazyImage(url: URL(string: icon)!) { state in
+                if let image = state.image {
+                    image.resizable()
+                } else {
+                    Color.gray
+                }
+            }
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 50, height: 50)
+            .clipShape(.rect(cornerRadius: 12))
+            
+            VStack(alignment: .leading) {
+                Text(title)
+                    .font(.headline)
+                    .foregroundStyle(.primary)
+                
+                Text(subtitlte)
+                    .font(.subheadline)
+                    .foregroundStyle(.secondary)
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Button(action: downloadAction) {
+                Text("Install")
+                    .font(.subheadline)
+                    .fontWeight(.medium)
+            }
+            .controlSize(.regular)
+            .apply {
+                if #available(iOS 26.0, macOS 26.0, watchOS 26.0, tvOS 26.0, *) {
+                    $0
+                      .tint(.accentColor)
+                      .buttonStyle(.glass)
+                } else {
+                    $0.buttonStyle(.bordered)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    List {
+        AppRowView(
+            title: "Seebaro",
+            subtitlte: "The Store",
+            icon: "https://seebaro.ir/apple-touch-icon.png",
+            downloadAction: {}
+        )
+    }
+}
