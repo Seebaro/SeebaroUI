108b04f56f0e27116b8f87e3e0c68d102c83c1a5
Armin <armin536@gmail.com>
2025-10-21 21:36:12 +0330

    Updated EmptyStateView

--- Sources/Views/EmptyState/EmptyStateView.swift
+++ Sources/Views/EmptyState/EmptyStateView.swift
@@ -8,91 +8,62 @@
 import SwiftUI
 
 public struct EmptyStateView: View {
-    
     public init(
         icon: String,
-        title: LocalizedStringKey,
-        description: LocalizedStringKey,
-        showAction: Bool = true,
-        actionTitle: LocalizedStringKey? = nil,
+        title: LocalizedStringResource,
+        description: LocalizedStringResource,
+        actionTitle: LocalizedStringResource? = nil,
         action: (() -> Void)? = nil
     ) {
         self.icon = icon
         self.title = title
         self.description = description
-        self.showAction = showAction
         self.actionTitle = actionTitle
         self.action = action
     }
     
-    public var icon: String
-    public var title: LocalizedStringKey
-    public var description: LocalizedStringKey
+    let icon: String
+    let title: LocalizedStringResource
+    let description: LocalizedStringResource
     
-    public var showAction: Bool = true
-    public var actionTitle: LocalizedStringKey? = nil
-    public var action: (() -> Void)? = nil
+    let actionTitle: LocalizedStringResource?
+    let action: (() -> Void)?
     
     public var body: some View {
-        Group {
-            if #available(iOS 17.0, macOS 14.0, visionOS 1.0, watchOS 10.0, *) {
-                ContentUnavailableView {
-                    Label(title, systemImage: icon)
-                        #if os(watchOS)
-                        .font(.title3)
-                        #else
-                        .font(.largeTitle)
-                        #endif
-                } description: {
-                    Text(description)
-                        #if os(watchOS)
-                        .font(.body)
-                        #else
-                        .font(.headline)
-                        #endif
-                } actions: {
-                    if let actionTitle, let action, showAction {
-                        Button(action: action) {
-                            Text(actionTitle)
-                                .font(.body)
-                                #if os(watchOS)
-                                .padding(.horizontal)
-                                #endif
-                        }
-                        #if os(watchOS)
-                        .tint(.accentColor)
-                        .buttonStyle(.bordered)
-                        #endif
+        ContentUnavailableView {
+            Label(title, systemImage: icon)
+                #if os(watchOS)
+                .font(.title3)
+                #else
+                .font(.largeTitle)
+                #endif
+                .symbolRenderingMode(.multicolor)
+                .apply {
+                    if #available(iOS 26.0, macOS 26.0, watchOS 26.0, tvOS 26.0, *) {
+                        $0.symbolColorRenderingMode(.gradient)
                     }
                 }
-            } else {
-                backwardView
+        } description: {
+            Text(description)
+                #if os(watchOS)
+                .font(.body)
+                #else
+                .font(.headline)
+                #endif
+        } actions: {
+            if let actionTitle, let action {
+                Button(action: action) {
+                    Text(actionTitle)
+                        .font(.body)
+                        #if os(watchOS)
+                        .padding(.horizontal)
+                        #endif
+                }
+                #if os(watchOS)
+                .tint(.accentColor)
+                .buttonStyle(.bordered)
+                #endif
             }
-        }
-    }
-    
-    var backwardView: some View {
-        VStack {
-            Image(systemName: icon)
-                #if os(iOS)
-                .font(.largeTitle)
-                .dynamicTypeSize(.accessibility2)
-                .foregroundStyle(.secondary)
-                #else
-                .font(.system(size: 36))
-                .foregroundStyle(.tertiary)
-                #endif
-            
-            Text(title)
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .multilineTextAlignment(.center)
-                .padding(.all, 12)
-                #if os(iOS)
-                .foregroundStyle(.primary)
-                #else
-                .foregroundStyle(.secondary)
-                #endif
         }
     }
 }
@@ -102,7 +73,6 @@ public struct EmptyStateView: View {
         icon: "swift",
         title: "Header title",
         description: "Some description for the empty state",
-        showAction: true,
         actionTitle: "An action title",
         action: {}
     )
